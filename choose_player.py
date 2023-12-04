4741a6b37c1ed94d1d140c50cd7da321e6a53a35
Tom Forde <tforde@atmospheremxt.com>
2023-12-04 21:28:48 +0000

    removed options, help

--- choose_player.py
+++
@@ -1,3 +0,0 @@
-from 
-
-def choose_player():
--- help.py
+++
@@ -1,9 +0,0 @@
-from main_menu import MainMenu
-
-MAIN_MENU = MainMenu()
-
-def show_help():
-    print("----------------------------")
-    print ("Coming Soon!")
-    print("----------------------------")
-    MAIN_MENU.show_menu()
